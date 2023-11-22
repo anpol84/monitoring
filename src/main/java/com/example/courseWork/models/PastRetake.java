62f0de89b7acb4e39f64d1659e3af388c67b24cd
anpol84 <polakovandrej327@mail.ru>
2023-11-22 14:26:47 +0300

    Initial commit

---
+++ src/main/java/com/example/courseWork/models/PastRetake.java
@@ -0,0 +1,32 @@
+package com.example.courseWork.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "past_retake")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PastRetake {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    private Date date;
+
+    private String result;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private String subject;
+
+
+}
